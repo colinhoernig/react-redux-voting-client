771e6cbdb92aafe06a39cae815c03f18ee7f57f8
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-17 21:09:31 -0500

    Split into Vote and Winner subcomponents.

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ import Voting from './components/Voting';
 const pair = ['Jeremy Jones', 'Alex Andrews'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} hasVoted="Jeremy Jones" />,
   document.getElementById('app')
 );
--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -2,7 +2,7 @@ import React from 'react/addons';
 import Voting from '../../src/components/Voting';
 import { expect } from 'chai';
 
-const { renderIntoDocument, scryRenderedDOMComponentsWithTag } = React.addons.TestUtils;
+const { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } = React.addons.TestUtils;
 
 describe('Voting', () => {
 
@@ -15,6 +15,19 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Jeremy Jones');
     expect(buttons[1].textContent).to.equal('Alex Andrews');
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={["Jeremy Jones", "Alex Andrews"]} vote={vote} />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Jeremy Jones');
   });
 
 });
