2194c6eaabc39ccd8c0b09b25c6ff705737a5623
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-19 23:15:54 -0500

    Apply react-redux to Results component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { List, Map } from 'immutable';
-
-const pair = List.of('Jeremy Jones', 'Alex Andrews');
-const tally = Map({ 'Jeremy Jones': 5, 'Alex Andrews': 3 });
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, { pair: pair, tally: tally });
+    return this.props.children;
   }
 });
--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react/addons';
 import ReactDOM from 'react-dom';
 import { List, Map } from 'immutable';
-import Results from '../../src/components/Results';
+import { Results } from '../../src/components/Results';
 import { expect } from 'chai';
 
 const { renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate } = React.addons.TestUtils;
