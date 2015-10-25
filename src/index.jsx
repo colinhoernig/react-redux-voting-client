3c0a0790e07fbde4dd7cdf07ff6959f8052648e1
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-24 23:35:54 -0400

    Create store before socket.

--- src/index.jsx
+++ src/index.jsx
@@ -11,15 +11,15 @@ import App from './components/App';
 import { VotingContainer } from './components/Voting';
 import { ResultsContainer } from './components/Results';
 
-const socket = io(`${location.protocol}//${location.hostname}:8001`);
-socket.on('state', state => {
-  store.dispatch(setState(state));
-});
-
 const createStoreWithMiddleware = applyMiddleware(
   remoteActionMiddleware(socket)
 )(createStore);
 const store = createStoreWithMiddleware(reducer);
+
+const socket = io(`${location.protocol}//${location.hostname}:8001`);
+socket.on('state', state => {
+  store.dispatch(setState(state));
+});
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
