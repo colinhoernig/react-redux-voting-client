1983d40bfd154bb2bbe7e2bbb61f3a8725903653
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-24 23:59:09 -0400

    Add comments to reducer. Fix lint errors.

--- src/reducer.js
+++ src/reducer.js
@@ -1,35 +1,51 @@
+/**
+ * The exported reducer function takes a state object and action,
+ * and modifies the state and returns the updated state.
+ */
+
 import { Map, List } from 'immutable';
 
+/**
+ * Update state by merging another state object
+ */
 function setState(state, newState) {
   return state.merge(newState);
 }
 
+/**
+ * Vote for a specific entry
+ */
 function vote(state, entry) {
   const currentPair = state.getIn(['vote', 'pair']);
   if (currentPair && currentPair.includes(entry)) {
     return state.set('hasVoted', entry);
-  } else {
-    return state;
   }
+  return state;
 }
 
+/**
+ * Reset the vote
+ */
 function resetVote(state) {
   const hasVoted = state.get('hasVoted');
-  const currentPair = state.getIn(['vote', 'pair'], List());
+  const currentPair = state.getIn(['vote', 'pair'], new List());
   if (hasVoted && !currentPair.includes(hasVoted)) {
     return state.remove('hasVoted');
-  } else {
-    return state;
   }
+  return state;
 }
 
-export default function(state = Map(), action) {
-  switch(action.type) {
+/**
+ * Main reducer function takes a state and action, and modifies
+ * the state, returning the updated state.
+ */
+export default function(state = new Map(), action) {
+  switch (action.type) {
     case 'SET_STATE':
       return resetVote(setState(state, action.state));
     case 'VOTE':
       return vote(state, action.entry);
+    default:
+      return state;
   }
-
-  return state;
 }
